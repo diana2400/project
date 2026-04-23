20e12733e2b4df1cb9348cb44e27f890798544e0
diana2400 <klote2000@mail.ru>
2026-04-23 12:04:01 +0300

    Добавил функцию угадывания числа

--- test.py
+++ test.py
@@ -10,5 +10,16 @@ if coin == 0:
 else:
     print("Выпал ОРЁЛ! 🪙")
 
-# Важный комментарий:
-# Мы изменили файл. Git это заметит!
+# main.py (добавь в конец)
+
+def guess_number():                     # новая функция
+    secret = random.randint(1, 5)
+    print("Угадай число от 1 до 5!")
+    guess = int(input("Твой ответ: "))
+    if guess == secret:
+        print("УРА! Ты угадал! 🎉")
+    else:
+        print(f"Не угадал. Было число {secret}")
+
+# Вызываем функцию
+guess_number()
